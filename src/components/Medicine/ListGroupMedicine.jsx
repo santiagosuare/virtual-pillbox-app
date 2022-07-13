a3e65c92b03d6d0b10f2563ef180d3d82e939498
Malaquias <mdejtiar@nerdwallet.com>
2022-07-13 05:50:24 -0300

    updated listgroup

--- src/components/Medicine/ListGroupMedicine.jsx
+++ src/components/Medicine/ListGroupMedicine.jsx
@@ -1,56 +1,36 @@
-import React from "react";
-import { ListGroup, Image, Container, Row, Col } from "react-bootstrap";
-import MOCK from "../../mocks/MOCK_DATA.json";
+import React, { useEffect } from "react";
+import { ListGroup, Container } from "react-bootstrap";
+import MedicineCard from "../MedicineCard";
+import { CircularProgress } from "@mui/material";
 
 const ListGroupMedicine = () => {
-  console.log(MOCK[0]);
+  const [ isLoading, setIsLoading ] = React.useState(true);
+  const [ medicines, setMedicines ] = React.useState([]);
+  useEffect(() => {
+    console.log('fecthing medicines');
+    fetch('http://localhost:5001/medicines', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 1,
+        },
+    }).then(async response => {
+      const result = await response.json();
+      setMedicines(result);
+    }).catch(error => {
+      console.log(error.message, 'error');
+    }).finally(() => setIsLoading(false))}, []);
 
   return (
-    <div>
       <ListGroup>
         <Container>
-          {MOCK.map((medicament) => (
-            <ListGroup.Item>
-              <Row>
-                <Col xs={6} md={4}>
-                  <h5>{medicament.name_drug}</h5>
-                  <p>Dosis: {medicament.cant} comprimidos</p>
-                  <p>
-                    <h4>
-                      <b>{medicament.time} HS</b>
-                    </h4>
-                  </p>
-                </Col>
-                <Col xs={3} md={2}>
-                  <Image
-                    src={medicament.image}
-                    className="img-circle"
-                    alt="Cinque Terre"
-                    width="100"
-                    height="100"
-                  />
-                </Col>
-              </Row>
-              {/* <div className="row" key={medicament.id}>
-                <div className="col-md-1">
-                  <h5>{medicament.name_drug}</h5>
-                  <p>{medicament.time}</p>
-                </div>
-              </div>
-              <div className="col-md-12">
-                <Image
-                  src={medicament.image}
-                  className="img-circle"
-                  alt="Cinque Terre"
-                  width="50"
-                  height="50"
-                />
-              </div> */}
-            </ListGroup.Item>
+          {
+            isLoading ? <CircularProgress /> :
+          medicines.map((medicament, index) => (
+            <MedicineCard key={index}/>
           ))}
         </Container>
       </ListGroup>
-    </div>
   );
 };
 
